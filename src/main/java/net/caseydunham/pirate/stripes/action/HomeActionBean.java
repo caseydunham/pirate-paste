a13ba09e77ee9e9742ac0daf0ec37f9cc70ba478
Casey Dunham <casey.dunham@gmail.com>
2013-01-19 12:08:54 -0500

    added slf4j and log4j logging configuration

--- src/main/java/net/caseydunham/pirate/stripes/action/HomeActionBean.java
+++ src/main/java/net/caseydunham/pirate/stripes/action/HomeActionBean.java
@@ -1,15 +1,20 @@
 package net.caseydunham.pirate.stripes.action;
 
 import net.sourceforge.stripes.action.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @UrlBinding("/home")
 public class HomeActionBean implements ActionBean {
+
+	private static final Logger LOG = LoggerFactory.getLogger(HomeActionBean.class);
 
 	private ActionBeanContext context;
 
 	@DontValidate
 	@DefaultHandler
 	public Resolution view() {
+		LOG.info("testing log setup");
 		return new ForwardResolution("/WEB-INF/jsps/Home.jsp");
 	}
 
