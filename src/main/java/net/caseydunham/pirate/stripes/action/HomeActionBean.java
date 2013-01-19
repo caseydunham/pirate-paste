6e60a3db4fb4c1e1cdc0634516fc7274e7919883
Casey Dunham <casey.dunham@gmail.com>
2013-01-19 12:13:23 -0500

    cleaning up HomeAction

--- src/main/java/net/caseydunham/pirate/stripes/action/HomeActionBean.java
+++ src/main/java/net/caseydunham/pirate/stripes/action/HomeActionBean.java
@@ -4,23 +4,26 @@ import net.sourceforge.stripes.action.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@UrlBinding("/home")
+@UrlBinding(HomeActionBean.HOME_ACTION)
 public class HomeActionBean implements ActionBean {
 
 	private static final Logger LOG = LoggerFactory.getLogger(HomeActionBean.class);
+
+	public static final String PAGE_LOCATION = "/WEB-INF/jsps";
+
+	public static final String HOME_ACTION = "/home";
+	public static final String HOME_PAGE = PAGE_LOCATION + "/Home.jsp";
 
 	private ActionBeanContext context;
 
 	@DontValidate
 	@DefaultHandler
 	public Resolution view() {
-		LOG.info("testing log setup");
-		return new ForwardResolution("/WEB-INF/jsps/Home.jsp");
+		LOG.info("forwarding to " + HOME_PAGE);
+		return new ForwardResolution(HOME_PAGE);
 	}
 
-	@Override
 	public void setContext(ActionBeanContext context) { this.context = context;	}
-	@Override
 	public ActionBeanContext getContext() { return context; }
 
 }
