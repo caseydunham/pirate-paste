cce21d49c42dd5570d0f24654b19795d94319946
Casey Dunham <casey.dunham@gmail.com>
2013-01-27 17:38:17 -0500

    more tests for view paste action bean

--- src/test/java/net/caseydunham/pirate/TestViewPasteActionBean.java
+++ src/test/java/net/caseydunham/pirate/TestViewPasteActionBean.java
@@ -9,10 +9,20 @@ import static org.junit.Assert.assertEquals;
 public class TestViewPasteActionBean extends BaseTest {
 
 	@Test
-	public void testHomeActionBeanSetsRightPage() throws Exception {
+	public void testViewActionBeanSetsRightPage() throws Exception {
 		MockRoundtrip trip = new MockRoundtrip(mockServletContext, ViewPasteActionBean.class, mockHttpSession);
 		trip.execute();
 		assertEquals("/WEB-INF/jsps/ViewPaste.jsp", trip.getDestination());
+	}
+
+	@Test
+	public void testViewSetsCorrectPaste() throws Exception {
+		MockRoundtrip trip = new MockRoundtrip(mockServletContext, ViewPasteActionBean.class, mockHttpSession);
+		trip.setParameter("id", "1");
+		trip.execute();
+
+		ViewPasteActionBean bean = trip.getActionBean(ViewPasteActionBean.class);
+		assertEquals(new Long(1), bean.getId());
 	}
 
 }
