3e19e7803bffcbacfa495085b4bbf8215415d360
Casey Dunham <casey.dunham@gmail.com>
2013-01-27 19:08:20 -0500

    adding TestViewActionBean to all tests suite

--- src/test/java/net/caseydunham/pirate/AllTests.java
+++ src/test/java/net/caseydunham/pirate/AllTests.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TestPasteService.class, TestHomeActionBean.class})
+@Suite.SuiteClasses({TestPasteService.class, TestHomeActionBean.class, TestViewPasteActionBean.class})
 public class AllTests {
 }
